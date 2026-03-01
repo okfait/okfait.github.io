cf8e89c863d7f7c30118172625264cc5012e7b75
okmusic generator <okmusic@localhost>
2026-03-01 16:34:38 +0200

    v2.9.1: Support Spotify Refresh Token

--- okmusic-api/api/index.js
+++ okmusic-api/api/index.js
@@ -16,6 +16,7 @@ async function initSpotify() {
                 spotify: {
                     client_id: process.env.SPOTIFY_CLIENT_ID,
                     client_secret: process.env.SPOTIFY_CLIENT_SECRET,
+                    refresh_token: process.env.SPOTIFY_REFRESH_TOKEN || undefined,
                     market: 'US'
                 }
             });
